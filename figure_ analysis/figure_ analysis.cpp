b7e89583756031e7e0680833387bafac51e052eb
hao <842658636@qq.com>
2019-05-27 23:57:06 +0800

    用sin（x）/x实验

--- figure_ analysis/figure_ analysis.cpp
+++ figure_ analysis/figure_ analysis.cpp
@@ -1,21 +1,96 @@
-﻿// figure_ analysis.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
-#include "pch.h"
+﻿#include "pch.h"
+#include "global.h"
 #include <iostream>
+#include <math.h>
+using namespace std;
+
+void LongBeiGe(double x0,double x1, double(*ptest)(double));
+double test(double x);
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	LongBeiGe(0.000000001,1, test);
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
+//测试的多项式g(x)
+double test(double x) {
+	return sin(x)/x;
+}
 
-// 入门提示: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
+//用龙贝格算式求积分 a:左端点，b：右端点 e
+void LongBeiGe(double a, double b,double(*ptest)(double)) {
+	double T1=0, T2=0, S1=0, S2=0, C1=0, C2=0, R1=0, R2=0, S;
+	R1 = 0; R2 = R1 + 2 * ACCURACY;
+	double h = b - a;//步长
+    T1 = h / 2.0*(ptest(a) + ptest(b));//梯形积分
+	int k = 1;//迭代次数
+	
+	while (abs(R1 - R2) >= ACCURACY) {
+		S = 0;
+		for (double x = a + h / 2.0; x < b; x += h) {
+			S += ptest(x);
+		}
+		T2 = T1 / 2.0 + S * h / 2.0;
+		if (k == 1) {
+			cout << "T" << k << ": " << T1 << endl;
+			cout << "T" << k + 1 << ": " << T2 << endl;
+		}
+		else {
+			cout << "T" << k + 1 << ": " << T2 << endl;
+		}
+		
+		S2 = T2 + (T2 - T1) / 3.0;
+		if (k == 1) {
+			//cout << "S" << k << ": " << S1 << endl;
+			cout << "S" << k + 1 << ": " << S2 << endl;
+		}
+		else {
+			cout << "S" << k + 1 << ": " << S2 << endl;
+		}
+		if (k == 1) {
+			k++;
+			h /= 2.0;
+			T1 = T2;
+			S1 = S2;
+			continue;
+		}
+		C2 = S2 + (S2 - S1) / 15.0;
+		if (k == 1) {
+			cout << "C" << k << ": " << C1 << endl;
+			cout << "C" << k + 1 << ": " << C2 << endl;
+		}
+		else {
+			cout << "C" << k + 1 << ": " << C2 << endl;
+		}
+		if (k == 2) {
+			C1 = C2;
+			k++;
+			h /= 2.0;
+			T1 = T2;
+			S1 = S2;
+			continue;
+		}
+		R2 = C2 + (C2 - C1) / 63.0;
+		if (k == 1) {
+			cout << "R" << k << ": " << R1 << endl;
+			cout << "R" << k + 1 << ": " << R2 << endl;
+		}
+		else {
+			cout << "R" << k + 1 << ": " << R2 << endl;
+		}
+		if (k == 3) {
+			R1 = R2;
+			C1 = C2;
+			k++;
+			h /= 2.0;
+			T1 = T2;
+			S1 = S2;
+			continue;
+		}
+
+
+	
+	}
+	cout << R2 << endl;
+}
+
---
+++ figure_ analysis/global.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#define ACCURACY 0.0001//精度
+#define N 10//迭代次数
